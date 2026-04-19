1d0f247aaae3320f11952da3c9e641e7068e8d64
Arun Sharma <arunsharma@Aruns-MacBook-Pro.local>
2026-04-19 22:45:02 +0530

    feat(swap): register FfioModule in AppModule

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '@workspace/auth';
 import { ChangeNowModule } from '@workspace/change-now';
 import { ConfigModule } from '@workspace/config';
+import { FfioModule } from '@workspace/ffio';
 import { FilesModule } from '@workspace/files-api/lib/files.module';
 import { IPUALogModule } from '@workspace/ipualog';
 import { MysqlDbModule } from '@workspace/mysql-db';
 import { SocketIoModule } from '@workspace/socket-io';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 @Module({
   imports: [
     MysqlDbModule,
     ChangeNowModule,
+    FfioModule,
     ConfigModule,
     SocketIoModule,
     FilesModule,
